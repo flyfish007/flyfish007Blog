d02c3e814fd3d6300515d8800df3dea6d8303283
guofangfang <252186302@qq.com>
2018-01-22 18:28:31 +0800

    vuex实验

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>  <a @click="submit">退出</a>  </div>
+    <div style="padding: 50px;">{{user.name}}欢迎回家</div>
+  </div>
+</template>
+<script>
+  import { mapState } from 'vuex'
+  import { mapActions } from 'vuex'
+  import { USER_SIGNOUT } from '../store/user'
+  export default {
+    computed: mapState({ user: state => state.user }),
+    methods: {
+      ...mapActions([USER_SIGNOUT]),
+      submit() {
+        this.USER_SIGNOUT()
+        this.$router.replace({ path: '/' })
+      }
+    }
+  }
+</script>
---
+++ src/components/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+
+    <form v-on:submit.prevent="submit">
+      <div class="line">
+        <div v-show="btn && !form.id">id不能为空</div>
+        <input type="number" placeholder="输入你的id" v-model="form.id">
+      </div>
+      <div class="line">
+        <div v-show="btn && !form.name">用户名不能为空</div>
+        <input type="text" placeholder="输入你的用户名" v-model="form.name">
+      </div>
+      <button>登录</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  import { USER_SIGNIN } from '../store/user'
+
+  export default {
+  name: 'login',
+  data() {
+    return {
+      btn: false, //true 已经提交过， false没有提交过
+      form: {
+        id: '',
+        name: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions([USER_SIGNIN]),
+    submit() {
+      this.btn = true
+      if(!this.form.id || !this.form.name) return
+      this.USER_SIGNIN(this.form)
+      this.$router.replace({ path: '/home' })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
